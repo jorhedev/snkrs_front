ecd915c6eea4446188cd88c864ae30e0e627c737
jorhedev <hijorhe@gmail.com>
2023-09-27 11:31:52 -0600

    Home done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import ListadoBusqueda from './views/ListodoBusqueda/ListadoBusqueda'
+import Navbar from "./components/Navbar/Navbar";
 import './App.css'
 
 import Home from './views/Home/Home'
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+          <Navbar/>
           <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/card" element={<ListadoBusqueda/>} />
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,21 +1,21 @@
 import styles from "./Home.module.css";
-import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import Promo from "../../components/Promo/Promo";
 import TopSales from "../../components/TopSales/TopSales";
 import Newsletter from "../../components/Newsletter/Newsletter";
-
+import BeMember from '../../components/BeMember/BeMember'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () =>{
 
     return(
         <div className={styles.container}>
-            <Navbar/>
             <Header/>
             <Promo/>
             <TopSales/>
+            <BeMember/>
             <Newsletter/>
-                <p>loading...</p>
+            <Footer/>
         </div>
     )
 }
