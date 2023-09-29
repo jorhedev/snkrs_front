ed7ba8c465901b0bf5d7054fabe25245a78e3545
Jorge Tolentino <132086547+jorhedev@users.noreply.github.com>
2023-09-28 18:38:05 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
+import Register from './components/Register/Register';
+import Detail from './components/Detail/Detail'
+import Navbar from "./components/Navbar/Navbar";
 import ListadoBusqueda from './views/ListodoBusqueda/ListadoBusqueda'
 import ListaBusqWomen from './views/ListodoBusqueda/ListaBusqWomen'
 import ListaBusqKids from './views/ListodoBusqueda/ListaBusqKids'
@@ -12,11 +15,14 @@ function App() {
 
   return (
     <>
+          <Navbar/>
           <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/card" element={<ListadoBusqueda/>} />
           <Route path="/cardw" element={<ListaBusqWomen/>} />
           <Route path="/cardk" element={<ListaBusqKids/>} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/register" element={<Register />} />
           </Routes>
       
     </>
