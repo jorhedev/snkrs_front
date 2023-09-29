728a39c1d202e493b1b1270e9d17c67a214bf76c
Karol Castilla Cruz <karolcastilla@hotmail.com>
2023-09-28 20:50:45 -0500

    Carrito de compras

---
+++ src/components/DashboardUser/RenderView.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
+import styles from "./RenderView.module.css"
+
+
+// eslint-disable-next-line react/prop-types
+const RenderView = ({ selectedView }) => {
+
+  switch (selectedView) {
+    case 'profile':
+      return (
+        <div>
+          <h1>This is profile</h1>
+        </div>
+      );
+    case 'favorites':
+      return (
+        <div>
+         <h1>this is favorites</h1>
+        </div>
+      );
+    case 'shopping':
+      return (
+        <div>
+          <ShoppingCart/>
+        </div>
+      );
+    case 'record':
+      return (
+        <div>
+          <h1>this is record</h1>
+        </div>
+      );
+    default:
+      return null;
+  }
+};
+
+export default RenderView;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import filtersReducer from './filtersSlice';
 import zapatillasReducer from './zapatillasSlice';
 import userReducer from './user';
+import cartReducer from './cartSlice'
 
 const store = configureStore({
   reducer: {
     zapatillas: zapatillasReducer,
     filters: filtersReducer,
     user: userReducer,
+    cart: cartReducer,
   }
 });
 
